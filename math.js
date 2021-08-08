1ec33fd681df753f9c6cd7e4894047d9ac22f0c6
labibkhanofficialbd <labibkhan66410@gmail.com>
2021-08-08 15:39:37 +0600

    math

--- math.js
+++ math.js
@@ -3,3 +3,15 @@ const myNumber = -5;
 const output = Math.abs(myNumber);
 
 console.log(output);
+
+const myNumber2 = 6.6897;
+const output2 = Math.ceil(myNumber2);
+const output3 = Math.floor(myNumber2);
+const output4 = Math.round(myNumber2);
+const output5 = Math.random() * 10;
+const rounded = Math.floor(output5);
+
+console.log(output2);
+console.log(output3);
+console.log(output4);
+console.log(rounded);
